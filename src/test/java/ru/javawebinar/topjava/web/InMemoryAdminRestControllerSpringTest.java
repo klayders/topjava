848b881aa0a6ec85ae4951faa28f9a2adfd87426
klayder <klayder@mail.ru>
2018-07-15 20:16:33 +0300

    2 7 constructor injection

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
@@ -17,14 +17,14 @@ import java.util.Collection;
 
 import static ru.javawebinar.topjava.UserTestData.ADMIN;
 
-@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/spring-db.xml"})
+@ContextConfiguration("classpath:spring/spring-test.xml")
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
 
     @Autowired
     private AdminRestController controller;
 
-    @Autowired(required = false)
+    @Autowired
     private InMemoryUserRepositoryImpl repository;
 
     @Before

7c7c5a90daca319a54aac52c23cc6cf6ea7fbaee
klayder <klayder@mail.ru>
2018-09-03 11:24:00 +0300

    9 12 spring security test

---
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -0,0 +1,69 @@
+package ru.javawebinar.topjava.to;
+
+import org.hibernate.validator.constraints.Range;
+import ru.javawebinar.topjava.model.Meal;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class MealTo extends BaseTo implements Serializable {
+    @NotNull
+    private  LocalDateTime dateTime;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private  String description;
+    @NotNull
+    @Range(min = 10, max = 5000)
+    private  int calories;
+
+    public MealTo() {
+    }
+
+    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories) {
+        super(id);
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public Meal asNewMeal(){
+        return new Meal(null, dateTime, description, calories);
+    }
+    @Override
+    public String toString() {
+        return "MealTo{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", id=" + id +
+                '}';
+    }
+}
--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -4,6 +4,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.to.MealWithExceed;
 
 import java.time.LocalDate;
@@ -28,13 +29,10 @@ public class MealAjaxController extends AbstractMealController {
     }
 
     @PostMapping
-    public void createOrUpdate(@RequestParam("id") Integer id,
-                               @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
-                               @RequestParam("description") String description,
-                               @RequestParam("calories") int calories) {
-        Meal meal = new Meal(id, dateTime, description, calories);
-        if (meal.isNew()) {
-            super.create(meal);
+    public void createOrUpdate(MealTo mealTo) {
+
+        if (mealTo.isNew()) {
+            super.create(mealTo.asNewMeal());
         }
     }
 
